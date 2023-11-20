277f4cef9996713c03bb9ad2ecc3c3d81a652e12
sasha <lorion06@gmail.fr>
2023-11-20 16:19:37 +0100

    [FRONT] add Home page; add NavBar component

--- ft_transcendence/srcs/front/src/App.tsx
+++ ft_transcendence/srcs/front/src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
+  //Constante
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <head>
+        <title>Transcendence</title>
+        <meta name="description" content="transcendence"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+      </head>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
