677c6762b33223f7f5c9c9b6f2a227f1f1da9a49
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-12-01 08:47:55 +0100

    allow login for dev mode

--- srcs/back/src/auth/auth.controller.ts
+++ srcs/back/src/auth/auth.controller.ts
@@ -2,33 +2,30 @@ import {
   ClassSerializerInterceptor,
   Controller,
   Get,
-  Req,
+  Post,
   Res,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common'
-import { Request, Response } from 'express'
+import { Response } from 'express'
 import { ConfigService } from '@nestjs/config'
-import { User } from '@prisma/client'
-import { UserEntity } from 'src/users/entity/user.entity'
 import JwtTwoFaGuard from './jwt/jwt-2fa.guard'
-
-type RequestWithUser = Request & { user: User }
+import { faker } from '@faker-js/faker'
+import { UsersService } from 'src/users/users.service'
+import { JwtAuthService } from './jwt/jwt-auth.service'
+import { UserEntity } from 'src/users/entity/user.entity'
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
 export class AuthController {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UsersService,
+    private readonly jwtAuthService: JwtAuthService,
+  ) {}
   @Get()
   async auth(@Res() res: Response) {
     return res.redirect('/auth/42')
-  }
-
-  @Get('login')
-  @UseGuards(JwtTwoFaGuard)
-  async login(@Req() req: RequestWithUser) {
-    console.log(req.user)
-    return new UserEntity(req.user)
   }
 
   @Get('logout')
@@ -36,5 +33,25 @@ export class AuthController {
   async logout(@Res() res: Response) {
     res.clearCookie('jwt')
     res.json({ message: 'Logged out' })
+  }
+
+  @Post('dev')
+  async loginDev(@Res() res: Response) {
+    const user = await this.userService.create({
+      email: faker.internet.email(),
+      image: faker.image.avatar(),
+      username: faker.internet.userName(),
+    })
+    if (!user)
+      return res.redirect(
+        this.configService.getOrThrow<string>('FRONT_URL') + '/login',
+      )
+    const jwt = await this.jwtAuthService.login(user)
+    res.cookie('jwt', jwt, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+    })
+    res.json({ message: 'Logged in', user: new UserEntity(user) })
   }
 }
