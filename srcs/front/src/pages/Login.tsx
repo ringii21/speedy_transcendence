968e4c24657ee802e7f9d4eb809413669dabf6fa
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-12-01 08:47:55 +0100

    allow login for dev mode

--- srcs/front/src/pages/Login.tsx
+++ srcs/front/src/pages/Login.tsx
@@ -2,19 +2,31 @@ import React, { useEffect, useState } from 'react'
 import { Si42 } from 'react-icons/si'
 import { redirect } from 'react-router-dom'
 import { useAuth } from '../providers/AuthProvider'
+import httpInstance from '../utils/httpClient'
 
 const Login = () => {
-  const { user } = useAuth()
+  const { user, signin } = useAuth()
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     if (user) redirect('/')
   }, [user])
 
-  const onButtonClick = async (e: React.MouseEvent<HTMLElement>) => {
+  const login42Click = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault()
     setLoading(true)
     window.open('http://localhost:3000/api/auth/42', '_self')
+  }
+
+  const loginDevClick = async (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+    setLoading(true)
+    try {
+      await httpInstance().post('/api/auth/dev')
+      await signin()
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   return (
@@ -23,14 +35,28 @@ const Login = () => {
         <div className="hero-content text-center">
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">ft_transcendence</h1>
-            <button
-              onClick={onButtonClick}
-              className="btn btn-lg btn-primary mt-10"
-              disabled={loading}
-            >
-              <span className={loading ? 'loading loading-spinner' : ''}></span>
-              Login with <Si42 style={{ fontSize: '2em' }} />
-            </button>
+            <div className="grid">
+              <button
+                onClick={login42Click}
+                className="btn btn-lg btn-primary mt-10"
+                disabled={loading}
+              >
+                <span
+                  className={loading ? 'loading loading-spinner' : ''}
+                ></span>
+                Login with <Si42 style={{ fontSize: '2em' }} />
+              </button>
+              <button
+                onClick={loginDevClick}
+                className="btn btn-lg btn-secondary mt-10"
+                disabled={loading}
+              >
+                <span
+                  className={loading ? 'loading loading-spinner' : ''}
+                ></span>
+                Login dev
+              </button>
+            </div>
           </div>
         </div>
       </div>
