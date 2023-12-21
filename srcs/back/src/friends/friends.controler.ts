8bb611a6ae43bbace9c09ded2b67926cf0b63508
Sasha LORION <sasha@transcendence.transcendence>
2023-12-21 10:55:12 +0100

    [BACK] - import dto to friends.controler

--- srcs/back/src/friends/friends.controler.ts
+++ srcs/back/src/friends/friends.controler.ts
@@ -3,6 +3,9 @@ import { AuthGuard } from "@nestjs/passport";
 import { FriendsService } from "./friends.service";
 import { JwtPayload } from "../auth/jwt/jwt-auth.strategy";
 import { UserDecorator } from "../users/decorator/users.decorator";
+import { FriendshipRemovalDto } from './dto/friend-removal.dto'
+import { FriendshipSearchDto } from './dto/friend-search.dto'
+import { FriendsRequestDto } from './dto/friend-add.dto'
 
 @UseGuards(AuthGuard)
 @Controller('friends')
@@ -12,22 +15,22 @@ export class FriendsControler {
   ) { }
 
   @Post('new')
-  addNewFriend(@UserDecorator() user: JwtPayload, @Body() payload: { userId: number }) {
-    return this.friendsService.addFriend(user.sub, payload.userId)
+  addNewFriend(@UserDecorator() user: JwtPayload, @Body() payload: FriendsRequestDto) {
+    return this.friendsService.addFriend(user.sub, payload.friendId)
   }
 
-  @Get('remove')
-  removeFriend(@UserDecorator() user: JwtPayload, @Body() payload: { userId: number }) {
-    return this.friendsService.deleteFriend(user.sub, payload.userId)
+  @Post('remove')
+  removeFriend(@UserDecorator() user: JwtPayload, @Body() payload: FriendshipRemovalDto) {
+    return this.friendsService.deleteFriend(user.sub, payload.friendId)
   }
 
-  @Get('nonFriends')
-  getNonFriends(@UserDecorator() user: JwtPayload, @Body() payload: { userId: number }) {
-    return this.friendsService.isNotFriend(user.sub, payload.userId)
-  }
+  // @Get('nonFriends')
+  // getNonFriends(@UserDecorator() user: JwtPayload, @Body() payload: { userId: number }) {
+  //   return this.friendsService.isNotFriend(user.sub, payload.userId)
+  // }
 
   @Get('all')
-  getAllFriends(@UserDecorator() @Body() payload: { userId: number}) {
-    return this.friendsService.allFriends(payload.userId)
+  getAllFriends(@UserDecorator() @Body() payload: FriendshipSearchDto) {
+    return this.friendsService.allFriends(payload.id)
   }
 }
